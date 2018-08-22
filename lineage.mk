814f56b80946930d4502258dccfda5d7156b8922
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-08-22 17:55:38 +0200

    d852: update build description and fingerprint
    taken from the latest update V30h(V30h-BLM-CA) Marshmallow
    factoryversion = LGD852AT-00-V30h-BLM-CA-JUL-27-2016+0
    Android Security Patch level = 2016-07-01

--- lineage.mk
+++ lineage.mk
@@ -8,6 +8,6 @@ PRODUCT_NAME := lineage_d852
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_bell_ca" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1421650137 1421650137 release-keys"
+    PRIVATE_BUILD_DESC="g3_bell_ca-user 6.0 MRA58K 162091130443e release-keys"
 
-BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys"
+BUILD_FINGERPRINT="lge/g3_bell_ca/g3:6.0/MRA58K/162091130443e:user/release-keys"
