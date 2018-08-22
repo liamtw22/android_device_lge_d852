b74fc97675a6979e58fb3fa6083fd995187e1bfa
Bruno Martins <bgcngm@gmail.com>
2018-08-22 17:29:43 +0200

    d852: set matching build fingerprint for system, vendor and boot images
    This makes all build fingerprints match on system, vendor and boot and fixes vendor image mismatch. (https://i.imgur.com/EGEnKqj.jpg)
    Needed after https://review.lineageos.org/#/c/208868

--- lineage.mk
+++ lineage.mk
@@ -8,5 +8,6 @@ PRODUCT_NAME := lineage_d852
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_bell_ca" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1421650137 1421650137 release-keys"
+
+BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys"
