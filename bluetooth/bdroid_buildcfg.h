8b2c91a2f99aba81b7e0c4db8a1bd2e3a941381a
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-08-22 18:34:18 +0200

    d852: set correct bluetooth name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "LG G3 D850"
+#define BTM_DEF_LOCAL_NAME "LG G3 D852"
 
 #define BLE_VND_INCLUDED          TRUE
 #define BLUETOOTH_QTI_SW          TRUE
