a44f4c6933e16d870d6faaed5ab6d9957ed9d651
Aleksander Gencel <johnnylittleplanet@gmail.com>
2018-12-20 15:00:00 +0100

    PotatoProject for d852

--- potato_d852.mk
+++ potato_d852.mk
@@ -20,12 +20,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d852 device
 $(call inherit-product, device/lge/d852/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common POSP stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="hardStyl3r"
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d852
-PRODUCT_NAME := lineage_d852
+PRODUCT_NAME := potato_d852
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D852
 PRODUCT_MANUFACTURER := LGE
